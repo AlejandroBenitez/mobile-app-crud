c09e593bd586f66410bb9b23b59c295f0166ee21
Alejandro Benitez <alejandrobenitezcorp@outlook.com>
2022-01-03 16:26:11 -0300

    Se crean las screens, se inserta input group en la screen CreateUser, se inserta boton, se agregan algunos estilos

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator();
+
+import UserList from "./screens/UserList";
+import CreateUser from "./screens/CreateUser";
+import UserDetail from "./screens/UserDetail";
+
+function MyStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="CreateUser" component={CreateUser} />
+      <Stack.Screen name="UserList" component={UserList} />
+      <Stack.Screen name="UserDetail" component={UserDetail} />
+    </Stack.Navigator>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <MyStack />
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
